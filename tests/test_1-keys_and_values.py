cc32f6735a636ee5410e469a8f25e7a387eb36a9
AntonioRochaAZ <80586458+AntonioRochaAZ@users.noreply.github.com>
2026-07-18 22:22:09 +0200

    Trying to make CI work.

--- tests/test_1-keys_and_values.py
+++ tests/test_1-keys_and_values.py
@@ -1,21 +1,21 @@
 from utils import get_all_language_modules
 
-def test_keys():
-    """
-    This obviously does not work (python overwrites repeated entries).
-    Using JSON files and trying to read them does not work either...
-    TODO: Although with a JSON file I could parse it with re: 
-    re.findall(r'".*"\:\s*".*"\s*,?', txt)
-    """
-    module_list = get_all_language_modules()
-    for module in module_list:
-         for dic in [
-            module.dictionary,
-            module.traceback_dictionary,
-            module.pylyglot_internal_messages
-        ]:
-            key_list = list(dic.keys())
-            assert len(key_list) == len(set(key_list))
+# def test_keys():
+#     r"""
+#     This obviously does not work (python overwrites repeated entries).
+#     Using JSON files and trying to read them does not work either...
+#     TODO: Although with a JSON file I could parse it with re: 
+#     re.findall(r'".*"\:\s*".*"\s*,?', txt)
+#     """
+#     module_list = get_all_language_modules()
+#     for module in module_list:
+#          for dic in [
+#             module.dictionary,
+#             module.traceback_dictionary,
+#             module.pylyglot_internal_messages
+#         ]:
+#             key_list = list(dic.keys())
+#             assert len(key_list) == len(set(key_list))
 
 def test_values():
     """
