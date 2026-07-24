048d7b6d0f70fdc4fd9db2d280d3b5152acfec1f
AntonioRochaAZ <80586458+AntonioRochaAZ@users.noreply.github.com>
2026-07-24 23:44:12 +0200

    Updating example a little bit

--- examples/documentation/features_multilingual/tests.pt.py
+++ examples/documentation/features_multilingual/tests.pt.py
@@ -1,6 +1,6 @@
 # Defining a small function which returns "General Kenobi" 
 # if input is "Hello there"
-def função_teste(s: string):
+def função_teste(s: txt):
     se s.lower().strip() == "hello there":
         retornar "General Kenobi"
     senão:
